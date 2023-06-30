ac426eb7f057d2aad2a79d33cc29c6c06b071818
Cynniiee <107312306+Cynniiee@users.noreply.github.com>
2023-06-30 23:27:40 +0800

    offers 30 june

---
+++ client/src/pages/Offers.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Box, Typography } from '@mui/material';
+
+function Offers() {
+  return (
+    <Box>
+      <Typography variant="h5" sx={{ my: 2 }}>
+        Offers
+      </Typography>
+    </Box>
+  )
+}
+
+export default Offers
