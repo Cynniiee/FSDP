99799892d77834de50a103cd7e7a81434b38a4ce
Cynniiee <107312306+Cynniiee@users.noreply.github.com>
2023-07-02 16:40:36 +0800

    offers 2 july

--- client/src/pages/Offers.jsx
+++ client/src/pages/Offers.jsx
@@ -1,12 +1,55 @@
 import React, { useEffect, useState } from 'react';
-import { Box, Typography, Grid, Card, CardContent } from '@mui/material';
+import { Box, Typography, Grid, Card, CardContent, Input, IconButton } from '@mui/material';
 import http from '../http';
-import { AccessTime } from '@mui/icons-material';
+import { AccessTime, Search, Clear } from '@mui/icons-material';
 import dayjs from 'dayjs';
 import global from '../global';
 
 function Offers() {
   const [offersList, setOffersList] = useState([]);
+  const [search, setSearch] = useState('');
+
+  // onSearchChange sets the state based on the input value
+  const onSearchChange = (e) => {
+    setSearch(e.target.value);
+  };
+
+  // Function getOffers: call api to get all offers
+  const getOffers = () => {
+    http.get('/tutorial').then((res) => {
+      setTutorialList(res.data);
+    });
+  };
+  // Function searchTutorials: call api to search tutorials
+  const searchOffers = () => {
+    http.get(`/tutorial?search=${search}`).then((res) => {
+      setTutorialList(res.data);
+    });
+  };
+
+  // Function useEffect: call function getOffers
+  useEffect(() => {
+    getOffers();
+  }, []);
+
+  // Function onSeachKeyDown: when enter key is down, call function searchOffers
+  const onSearchKeyDown = (e) => {
+    if (e.key === "Enter") {
+      searchOffers();
+    }
+  };
+
+  // Function onClickSearch: call function searchOffers
+  const onClickSearch = () => {
+    searchOffers();
+  }
+
+  // Function onClickClear: clear search state and call function getOffers
+  const onClickClear = () => {
+    setSearch('');
+    getOffers();
+  };
+    
 
   useEffect(() => {
     http.get('/offers').then((res) => {
@@ -21,6 +64,19 @@ function Offers() {
         Offers
       </Typography>
 
+      <Box sx={{ display: 'flex', alignItems: 'center', mb: 2 }}>
+        <Input value={search} placeholder="Search" 
+         onChange={onSearchChange} onKeyDown={onSearchKeyDown}
+        />
+        <IconButton color="primary" onClick={onClickSearch}>
+          <Search />
+        </IconButton>
+
+        <IconButton color="primary" onClick={onClickClear}>
+          <Clear />
+        </IconButton>
+      </Box>
+
       <Grid container spacing={2}>
         {
           offersList.map((offers, i) => {
@@ -28,6 +84,7 @@ function Offers() {
               <Grid item xs={12} md={6} lg={4} key={offers.id}>
                 <Card>
                   <CardContent>
+
                     <Typography variant="h6" sx={{ mb: 1 }}>
                       {offers.brandName}
                     </Typography>
@@ -35,8 +92,9 @@ function Offers() {
                     <Box sx={{ display: 'flex', alignItems: 'center', mb: 1 }}
                       color="text.secondary">
                       <AccessTime sx={{ mr: 1 }} />
+
                       <Typography>
-                        {offers.createdAt}
+                        {dayjs(offers.createdAt).format(global.datetimeFormat)}
                       </Typography>
                     </Box>
 
